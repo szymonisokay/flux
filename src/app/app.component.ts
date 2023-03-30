4a441c40b0a8ff3c50757338c315fe292dfdf6c9
Szymon Wałach <72652252+szymonisokay@users.noreply.github.com>
2023-03-30 19:22:44 +0200

    modify files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import {
+  defaultLanguages,
+  TransalationLanguages,
+} from './shared/interfaces/translate-languages.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(defaultLanguages);
+    this.translateService.setDefaultLang(TransalationLanguages.EN);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,61 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { LoginModule } from './auth/feature/login/login.module';
+import { LoginRoutingModule } from './auth/feature/login/login-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SharedModule } from './shared/shared.module';
+import { RegisterModule } from './auth/feature/register/register.module';
+import { RegisterRoutingModule } from './auth/feature/register/register-routing.module';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    LoginModule,
+    LoginRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpsTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+    SharedModule,
+    RegisterModule,
+    RegisterRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function httpsTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
