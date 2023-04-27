7221eb671df9bd8b288c3ee3c929c68a6949d263
Szymon Wałach <72652252+szymonisokay@users.noreply.github.com>
2023-04-27 19:09:39 +0200

    add rapidapi key and host keys

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
   baseApiUrl: 'http://localhost:5001/api',
+  exercisesApiKey: 'cc06dbdb51msh6b01de7fdf02424p1aaaa7jsna1ec294b0962',
+  exercisesApiHost: 'musclewiki.p.rapidapi.com',
 };
